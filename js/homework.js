f8b8033c231173d935c194938ddf46f87be66059
Terry Kubalewski <terrykub@yahoo.com>
2023-01-19 17:10:53 -0600

    finished homework

--- js/homework.js
+++ js/homework.js
@@ -1,27 +1,39 @@
+console.log("======Exercise 1 - Closures=======")
 // Exercise 1 - Closures
 // Update the createAdder function below so that
 // the below code works as intended
 
-// function createAdder(){};
+function createAdder(x){
+    function addEm(num){
+        return num + x
+    }
+    return addEm
+};
 
-// const addEight = createAdder(8);
-// console.log(addEight(10)); // 18
-// console.log(addEight(17)); // 25
-// console.log(addEight(50)); // 58
-// console.log(addEight(100)); // 108
-// console.log(addEight(92)); // 100
 
-// const addThree = createAdder(3);
-// console.log(addThree(10)); // 13
-// console.log(addThree(17)); // 20
-// console.log(addThree(50)); // 53
-// console.log(addThree(100)); // 103
-// console.log(addThree(92)); // 95
+const addEight = createAdder(8);
+console.log(addEight(10)); // 18
+console.log(addEight(17)); // 25
+console.log(addEight(50)); // 58
+console.log(addEight(100)); // 108
+console.log(addEight(92)); // 100
 
+const addThree = createAdder(3);
+console.log(addThree(10)); // 13
+console.log(addThree(17)); // 20
+console.log(addThree(50)); // 53
+console.log(addThree(100)); // 103
+console.log(addThree(92)); // 95
+
+
+
+console.log("======Exercise 2 - Promises =======")
 
 // Exercise 2 - Promises 
-// Using the below getMovieInfo function, which is a Promised-base function, write an asynchronous function (.then().catch() or async/await)
-// called printMovieInfo that will take in a movie title and then either displays the movie information or logs an error with a console.warn().
+// Using the below getMovieInfo function, which is a Promised-base function, write an asynchronous function 
+// (.then().catch() or async/await)
+// called printMovieInfo that will take in a movie title and then either 
+// displays the movie information or logs an error with a console.warn().
 
 
 function getMovieInfo(movieName){
@@ -41,11 +53,19 @@ function getMovieInfo(movieName){
     })
 }
 
+async function printMovieInfo(movieName){
+    try{
+        let movie = await getMovieInfo(movieName)
+        console.log(`${movieName} directed by ${movie.director}. A story of ${movie.description} that runs for ${movie.runtime}`)
+    } catch(err) {
+        console.warn(err)
+    }
+};
 
 // Example 1
-// printMovieInfo('Indiana Jones and the Dark Knight')
+printMovieInfo('Indiana Jones and the Dark Knight')
 // Output: Indiana Jones and the Dark Knight directed by Christopher Spielberg. A story of Good vs Evil that runs for 157 minutes.
 
 // Example 2
-// printMovieInfo('ET')
+printMovieInfo('ET')
 // Output: *Warning* ET cannot be accessed because it it too short
